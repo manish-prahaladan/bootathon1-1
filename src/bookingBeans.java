3a1edc63871b004042b60e7a691be28dcc6374a4
Mohammed Safwan <mdsafwan3102001@gmail.com>
2021-08-16 19:29:06 +0530

    Added Time,Screen and Date Selection and a Javabeans file

---
+++ src/bookingBeans.java
@@ -0,0 +1,29 @@
+public class bookingBeans {
+    private String dayInfo;
+    private String timeInfo;
+    private String screenInfo;
+
+    public void setDayInfo(String dayInfo) {
+        this.dayInfo = dayInfo;
+    }
+
+    public String getDayInfo() {
+        return dayInfo;
+    }
+
+    public void setTimeInfo(String timeInfo) {
+        this.timeInfo = timeInfo;
+    }
+
+    public String getTimeInfo() {
+        return timeInfo;
+    }
+
+    public void setScreenInfo(String screenInfo) {
+        this.screenInfo = screenInfo;
+    }
+
+    public String getScreenInfo() {
+        return screenInfo;
+    }
+}
